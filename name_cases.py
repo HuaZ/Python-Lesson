cedc2cef44346a6459ccd19ddb641bba92f595f1
HuaZ <zh.zhuhua@gmail.com>
2017-09-13 19:46:44 +0800

    Create name_cases.py

---
+++ name_cases.py
@@ -0,0 +1,7 @@
+name = "eric"
+message = "Hello" + " " + name.title() + ",would you like to learn some Python today?"
+print(message)
+
+print(name.lower())
+print(name.upper())
+print(name.title())
